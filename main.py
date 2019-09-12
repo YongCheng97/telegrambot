d0e590110fe7678d92fe664140dca3b168b2cc0b
YongCheng97 <fooyongcheng97@hotmail.com>
2019-09-12 15:09:06 +0800

    'test'

---
+++ main.py
@@ -0,0 +1,120 @@
+import json 
+import requests
+import time
+import urllib
+import telegram
+import quickstart as qs
+
+TOKEN = "882567029:AAHiVaS_Th6XtfKR1Czg5y6eg7tsqy433ow"
+URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
+
+
+def get_json_from_url(url):
+    content = get_url(url)
+    js = json.loads(content)
+    return js
+
+
+def get_updates(offset=None):
+    url = URL + "getUpdates?timeout=100"
+    if offset:
+        url += "&offset={}".format(offset)
+    js = get_json_from_url(url)
+    return js
+
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
+
+def handle_updates(updates):
+    for update in updates["result"]:
+        text = update["message"]["text"]
+        message = update["message"]
+        chat = update["message"]["chat"]["id"]
+        items = ["Clock in", "Clock out"]
+        if text == "/start": 
+            send_message("Welcome to learnseeker! Please type /command to clock-in or clock-out", chat)
+        if text == "/command":
+            keyboard = build_keyboard(items)
+            send_message("Please choose an option", chat, keyboard)
+        if text == "Clock in":
+            f = open("userName.txt", "w")
+            f.write(str(message["from"]["first_name"].encode("utf-8")))
+            f.close()
+            f = open("clock.txt", "w")
+            f.write("Clock-in")
+            f.close()
+            qs.main()
+            send_message("Have a good day at work!", chat)
+        elif text == "Clock out": 
+            f = open("userName.txt", "w")
+            f.write(str(message["from"]["first_name"].encode("utf-8")))
+            f.close()
+            f = open("clock.txt", "w")
+            f.write("Clock-out")
+            f.close()
+            send_message("What did you do today? :) (Optional)", chat)
+        else : 
+            f = open("activities.txt", "w")
+            f.write(text)
+            f.close()
+            qs.main()
+            send_message("Goodbye!", chat)
+
+def arr():
+    from datetime import datetime
+    date = str(datetime.now().date())
+    time = str(datetime.now().time())
+    f = open("userName.txt", "r")
+    name = f.read()
+    f= open("clock.txt", "r")
+    clock = f.read()
+    f = open("activities.txt", "r")
+    activities = f.read()
+    if clock == "Clock-in":
+        return [name, date, "Clock-in", time]
+    elif clock == "Clock-out": 
+        return [name, date, "Clock-out", time, activities]
+
+def get_last_chat_id_and_text(updates):
+    num_updates = len(updates["result"])
+    last_update = num_updates - 1
+    text = updates["result"][last_update]["message"]["text"]
+    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+    return (text, chat_id)
+
+def send_message(text, chat_id):
+    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+    get_url(url)
+
+def send_message(text, chat_id, reply_markup=None):
+    text = urllib.parse.quote_plus(text)
+    url = URL + "sendMessage?text={}&chat_id={}&parse_mode=Markdown".format(text, chat_id)
+    if reply_markup:
+        url += "&reply_markup={}".format(reply_markup)
+    get_url(url)
+
+def build_keyboard(items):
+    keyboard = [[item] for item in items]
+    reply_markup = {"keyboard":keyboard, "one_time_keyboard": True}
+    return json.dumps(reply_markup)    
+
+def main():
+    last_update_id = None
+    while True:
+        updates = get_updates(last_update_id)
+        if len(updates["result"]) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            handle_updates(updates)
+        time.sleep(0.5)
+
+
+if __name__ == '__main__':
+    main()
---
+++ quickstart.py
@@ -0,0 +1,80 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from pprint import pprint
+import main as botmain
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+# The ID and range of a sample spreadsheet.
+spreadsheet_id = '1Xvfrph-td1ScQnavx9a8caDMYnugOM9bQ_1h0rujZ_A'
+range_ = 'October2019!A2:E6'
+
+# How the input data should be interpreted.
+value_input_option = 'RAW'  # TODO: Update placeholder value.
+
+# How the input data should be inserted.
+insert_data_option = 'INSERT_ROWS'  # TODO: Update placeholder value.
+
+def generateBody():
+    value_range_body = {
+        # TODO: Add desired entries to the request body.
+        "range": 'October2019!A2:E6',
+        "majorDimension": 'ROWS',
+        "values": [
+            botmain.arr()
+        ]
+    }
+    return value_range_body
+
+def main():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    #sheet = service.spreadsheets()
+    #result = sheet.values().get(spreadsheetId=spreadsheet_Id,
+                                #range=range_.execute()
+    #values = result.get('values', [])
+
+    request = service.spreadsheets().values().append(spreadsheetId=spreadsheet_id, range=range_, valueInputOption=value_input_option, insertDataOption=insert_data_option, body=generateBody())
+    response = request.execute()
+    pprint(response)
+    
+    """
+    if not values:
+        print('No data found.')
+    else:
+        print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            print('%s, %s' % (row[0], row[4]))
+    """
+
+if __name__ == '__main__':
+    main()
